e9b9968b305bb86fb647c7b013e41fa97e45c047
João Henrique Machado Silva <joao.henrique-silva@dazn.com>
2019-05-24 18:23:05 +0200

    finished http2 server and client with ssl

---
+++ http1/client/client.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	client := &http.Client{}
+
+	// Create a pool with the server certificate since it is not signed by a known CA
+	caCert, err := ioutil.ReadFile("../cert/server.crt")
+	if err != nil {
+		log.Fatalf("Error reading server certificate. %s", err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Create TLS configuration with the certificate of the server
+	tlsConfig := &tls.Config{
+		RootCAs: caCertPool,
+	}
+
+	// Use the proper transport in the client
+	client.Transport = &http.Transport{
+		TLSClientConfig: tlsConfig,
+	}
+
+	// Perform the request
+	res, err := client.Post("https://localhost:9191/hello/sayhello", "text/plain", bytes.NewBufferString("Hello Go!"))
+	if err != nil {
+		log.Fatalf("Request failed. %s", err)
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("Failed reading response body. %s", err)
+	}
+
+	fmt.Printf("Status Code: %d, Proto: %s, Body: %s", res.StatusCode, res.Proto, string(body))
+}
---
+++ http2/client/client.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"golang.org/x/net/http2"
+)
+
+func main() {
+	client := &http.Client{}
+
+	// Create a pool with the certificate, since it is not signed by any CA
+	caCert, err := ioutil.ReadFile("../cert/server.crt")
+	if err != nil {
+		log.Fatalf("Error reading certificate. %s", err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Create TLS configuration with certificate from server
+	tlsConfig := &tls.Config{
+		RootCAs: caCertPool,
+	}
+
+	// Use the proper transport in the client
+	client.Transport = &http2.Transport{
+		TLSClientConfig: tlsConfig,
+	}
+
+	// Perform the request
+	resp, err := client.Post("https://localhost:9191/hello/sayHello", "text/plain", bytes.NewBufferString("Hello Go!"))
+	if err != nil {
+		log.Fatalf("Failed get: %s", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed reading response body: %s", err)
+	}
+	fmt.Printf("Got response %d: %s %s", resp.StatusCode, resp.Proto, string(body))
+}
